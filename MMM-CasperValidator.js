523e517bc273188f375afaba8f4f8350f4b231ee
Toan Le <ktoanlba@gmail.com>
2021-10-16 16:28:38 +0700

    update

--- MMM-CasperValidator.js
+++ MMM-CasperValidator.js
@@ -21,18 +21,30 @@ Module.register("MMM-CasperValidator", {
 	start: function() {
 		var self = this;
 		var dataRequest = null;
-		var rewardDataRequest = null;
-		var nodeStatus = null;
+		this.stakedInfo = {selfStaked: 0, totalStaked: 0, active: false};
+		this.rewardDataRequest = null;
+		this.nodeStatus = {
+			theirNode: { era_id: 0, height: 0 },
+			ourNode: { era_id: 0, height: 0 }
+		};
 
 		//Flag for check if module is loaded
 		this.loaded = false;
 		this.loadingFailed = false;
 
 		// Schedule update timer.
-		this.getData();
+		this.requestUpdate();
 		setInterval(function() {
+			self.requestUpdate();
 			self.updateDom();
 		}, this.config.updateInterval);
+	},
+
+	requestUpdate: function() {
+		this.sendSocketNotification("MMM-CasperValidator-THEIR_NODE_STATUS", { nodeUrl: this.config.theirNode });
+		this.sendSocketNotification("MMM-CasperValidator-OUR_NODE_STATUS", { nodeUrl: this.config.ourNode });
+		this.sendSocketNotification("MMM-CasperValidator-REWARDS", { validatorAddress: this.config.validatorAddress });
+		this.sendSocketNotification("MMM-CasperValidator-GET_AUCTION_INFO", { validatorAddress: this.config.validatorAddress, nodeUrl: this.config.theirNode });
 	},
 
 	getLatestSerieValue: function(name, trunc, localeFormat) {
@@ -47,124 +59,6 @@ Module.register("MMM-CasperValidator", {
 		if (locale) v = new Number(v).toLocaleString("en-US");
 		return v;
 	},
-	/*
-	 * getData
-	 * function example return data and show it in the module wrapper
-	 * get a URL request
-	 *
-	 */
-	getData: function() {
-		var self = this;
-
-		var params = `query={__name__=~"casper_validator_self_staked_amount|casper_validator_total_staked_amount|casper_validator_is_active|casper_validator_should_be_upgraded",job="${this.config.prometheus_job}"}`;
-		var urlApi = `${this.config.api_url}/api/v1/query?${params}`;
-
-		var startTime = new Date();
-		startTime.setHours(startTime.getHours() - 10);
-		var endTime = new Date();
-		var rewardParams = `query=sum by(era_id)(casper_validator_era_rewards{job="${this.config.prometheus_job}"})&start=${startTime.toISOString()}&end=${endTime.toISOString()}&step=7200`;
-		var rewardUrlApi = `${this.config.api_url}/api/v1/query_range?${rewardParams}`;
-
-		var retry = true;
-
-		this.getRewardsData(self);
-		// this.getCasperOfficialRPC();
-
-		var dataRequest = new XMLHttpRequest();
-		dataRequest.open("GET", urlApi, true);
-		dataRequest.onreadystatechange = function() {
-			if (this.readyState === 4) {
-				if (this.status === 200) {
-					self.loadingFailed = false;
-					self.processData(JSON.parse(this.response));
-				} else if (this.status === 401) {
-					self.updateDom(self.config.animationSpeed);
-					Log.error(self.name, this.status);
-					retry = false;
-				} else {
-					self.loadingFailed = true;
-					self.updateDom(self.config.animationSpeed);
-					Log.error(self.name, "Could not load data.");
-				}
-				if (retry) {
-					self.scheduleUpdate((self.loaded) ? -1 : self.config.retryDelay);
-				}
-			}
-		};
-		dataRequest.send();
-	},
-
-	getCasperOfficialRPC: function() {
-		var self = this;
-		let request = new XMLHttpRequest();
-		let params = JSON.stringify({
-			id: "0",
-			jsonrpc: "2.0",
-			method: "info_get_status"
-		});
-
-		request.open("POST", 'https://node-clarity-mainnet.make.services/rpc', true);
-		request.setRequestHeader("Content-Type", "application/json");
-		request.setRequestHeader("User-Agent", "PostmanRuntime/7.28.4");
-		request.setRequestHeader("Accept", "*/*");
-		request.setRequestHeader("Accept-Encoding", "gzip, deflate, br");
-		request.setRequestHeader("Connection", "keep-alive");
-		
-		request.onreadystatechange = function() {
-			if (this.readyState === 4) {
-				if (this.status === 200) {
-					// self.loadingFailed = false;
-					// self.rewardDataRequest = JSON.parse(this.response);
-					// self.updateDom(self.config.animationSpeed);
-					console.log(this.response);
-				} else {
-					self.loadingFailed = true;
-					self.updateDom(self.config.animationSpeed);
-					Log.error(self.name, "Could not load data.");
-				}
-			}
-		};
-		request.send(params);
-	},
-
-	getRewardsData: function() {
-		var self = this;
-		let request = new XMLHttpRequest();
-		let rewardUrlApi = `https://event-store-api-clarity-mainnet.make.services/validators/${this.config.validatorAddress}/rewards?with_amounts_in_currency_id=1&page=1&limit=4&order_direction=DESC`;
-		request.open("GET", rewardUrlApi, true);
-		request.onreadystatechange = function() {
-			if (this.readyState === 4) {
-				if (this.status === 200) {
-					self.loadingFailed = false;
-					self.rewardDataRequest = JSON.parse(this.response);
-					self.updateDom(self.config.animationSpeed);
-				} else {
-					self.loadingFailed = true;
-					self.updateDom(self.config.animationSpeed);
-					Log.error(self.name, "Could not load data.");
-				}
-			}
-		};
-		request.send();
-	},
-
-	/* scheduleUpdate()
-	 * Schedule next update.
-	 *
-	 * argument delay number - Milliseconds before next update.
-	 *  If empty, this.config.updateInterval is used.
-	 */
-	scheduleUpdate: function(delay) {
-		var nextLoad = this.config.updateInterval;
-		if (typeof delay !== "undefined" && delay >= 0) {
-			nextLoad = delay;
-		}
-		nextLoad = nextLoad ;
-		var self = this;
-		setTimeout(function() {
-			self.getData();
-		}, nextLoad);
-	},
 
 	createTiles: function(field_name, trunc) {
 		trunc = (typeof trunc !== 'undefined') ? trunc : true;
@@ -176,7 +70,8 @@ Module.register("MMM-CasperValidator", {
 		return valueElement;
 	},
 
-	compareNodes: function(data) {
+	compareNodes: function() {
+		let data = this.nodeStatus;
 		let tileTable = document.createElement("table");
 		tileTable.className = "tiles";
 		tileTable.innerHTML = `
@@ -190,6 +85,10 @@ Module.register("MMM-CasperValidator", {
 				<td class="value ${this.compareCssClass(data.ourNode.height, data.theirNode.height)}">${data.ourNode.height}</td>
 				<td class="value green">${data.theirNode.height}</td>
 			</tr>
+			<tr>
+				<td>Node Status</td>
+				<td class="value ${this.compareCssClass(this.isActiveNode(), true)}" colspan="2">${this.isActiveNodeLabel()}</td>
+			</tr>
 		`;
 		
 		return tileTable;
@@ -199,28 +98,8 @@ Module.register("MMM-CasperValidator", {
 		return (a == b) ? "green" : "red";
 	},
 
-	activeStatusPanel: function() {
-		let tileElement = document.createElement("div");
-		tileElement.className = "tiles";
-
-		let status = this.getLatestSerieValue("casper_validator_is_active", false, false);
-
-		let valueElement = document.createElement("div");
-		valueElement.className = "value light align-center";
-
-		if (this.loadingFailed) {
-			valueElement.className += " red";
-			valueElement.innerHTML = "☠️ INTERNET FAILED ☠️";
-		} else if (status == "1") {
-			valueElement.className += " green";
-			valueElement.innerHTML = "☘️ ACTIVE VALIDATOR ☘️";
-		} else {
-			valueElement.className += " red";
-			valueElement.innerHTML = "☠️ NOT VALIDATOR ☠️";
-		}
-
-		tileElement.appendChild(valueElement);
-		return tileElement;
+	isActiveNodeLabel: function() {
+		return this.isActiveNode() ? "☘️ ACTIVE VALIDATOR ☘️" : "☠️ NOT VALIDATOR ☠️";
 	},
 	upgradeStatusPanel: function() {
 		let tileElement = document.createElement("div");
@@ -243,10 +122,6 @@ Module.register("MMM-CasperValidator", {
 		return tileElement;
 	},
 
-	recentRewardsPanel: function() {
-
-	},
-
 	parseRewardResults: function(json) {
 		let results = [];
 		
@@ -256,37 +131,34 @@ Module.register("MMM-CasperValidator", {
 
 		return results;
 	},
+
+	isActiveNode: function() {
+		return this.nodeStatus.ourNode.era_id > 0 && this.rewardDataRequest && this.rewardDataRequest.data[0].amount > 0 && this.stakedInfo.active
+	},
 	getDom: function() {
 		var self = this;
 
 		// create element wrapper for show into the module
 		var wrapper = document.createElement("div");
-		// If this.dataRequest is not empty
-		if (this.dataRequest) {
-			let tileTable = document.createElement("table");
-			let tileHeaderRow = document.createElement("tr");
-			tileHeaderRow.innerHTML = "<td class='bright align-center'>Total Self Staked</td><td></td><td class='bright align-center'>Total Staked</td>";
-			tileTable.appendChild(tileHeaderRow);
+		let tileTable = document.createElement("table");
+		tileTable.innerHTML = `
+			<tr>
+				<td class='bright align-center'>Total Self Staked</td>
+				<td></td>
+				<td class='bright align-center'>Total Staked</td>
+			</tr>
+			<tr class="tiles">
+				<td class="value number large align-center">${this.formatedCurrency(this.stakedInfo.selfStaked, true, true)}</td>
+				<td class="padding-td"></td>
+				<td class="value number large align-center">${this.formatedCurrency(this.stakedInfo.totalStaked, true, true)}</td>
+			</tr>
+		`;
+		wrapper.appendChild(tileTable);
 
-			let tileDataRow = document.createElement("tr");
-			tileDataRow.className = "tiles";
-			tileDataRow.appendChild(this.createTiles("casper_validator_self_staked_amount"));
-			let paddingTd = document.createElement("td");
-			paddingTd.className = "padding-td";
-			tileDataRow.appendChild(paddingTd);
-			tileDataRow.appendChild(this.createTiles("casper_validator_total_staked_amount"));
-			tileTable.appendChild(tileDataRow);
-			wrapper.appendChild(tileTable);
+		wrapper.appendChild(document.createElement("hr"));
+		wrapper.appendChild(this.compareNodes());
 
-			if (this.nodeStatus) {
-				wrapper.appendChild(document.createElement("hr"));
-				wrapper.appendChild(this.compareNodes(this.nodeStatus));
-			}
-
-			wrapper.appendChild(document.createElement("hr"));
-			wrapper.appendChild(this.activeStatusPanel());
-			wrapper.appendChild(this.upgradeStatusPanel());
-		}
+		// wrapper.appendChild(this.upgradeStatusPanel());
 
 		if (this.rewardDataRequest) {
 			wrapper.appendChild(document.createElement("hr"));
@@ -325,22 +197,26 @@ Module.register("MMM-CasperValidator", {
 		};
 	},
 
-	processData: function(data) {
-		var self = this;
-		this.dataRequest = data;
-		if (this.loaded === false) { self.updateDom(self.config.animationSpeed) ; }
-		this.loaded = true;
-
-		// the data if load
-		// send notification to helper
-		this.sendSocketNotification("MMM-CasperValidator-COMPARE_NODES", { theirNode: this.config.theirNode, ourNode: this.config.ourNode });
-	},
-
 	// socketNotificationReceived from helper
 	socketNotificationReceived: function (notification, payload) {
-		if(notification === "MMM-CasperValidator-COMPARE_NODES") {
-			this.nodeStatus = payload;
-			this.updateDom();
+		switch(notification) {
+			case "MMM-CasperValidator-THEIR_NODE_STATUS":
+				this.nodeStatus.theirNode = payload;
+				this.updateDom();
+				break;
+			case "MMM-CasperValidator-OUR_NODE_STATUS":
+				this.nodeStatus.ourNode = payload;
+				this.updateDom();
+				break;
+			case "MMM-CasperValidator-REWARDS":
+				this.rewardDataRequest = payload;
+				this.updateDom();
+				break;
+			case "MMM-CasperValidator-STAKED_INFO":
+				this.stakedInfo = payload;
+				console.log(payload);
+				this.updateDom();
+				break;
 		}
-	},
+	}
 });
--- node_helper.js
+++ node_helper.js
@@ -8,7 +8,6 @@
 var NodeHelper = require("node_helper");
 const axios = require('axios');
 
-
 module.exports = NodeHelper.create({
 
 	// Override socketNotificationReceived method.
@@ -20,20 +19,62 @@ module.exports = NodeHelper.create({
 	 * argument payload mixed - The payload of the notification.
 	 */
 	socketNotificationReceived: function(notification, payload) {
-		if (notification === "MMM-CasperValidator-COMPARE_NODES") {
-			this.compareBothNodes(payload);
+		switch(notification) {
+			case "MMM-CasperValidator-THEIR_NODE_STATUS":
+				this.sendNodeStatus(notification, payload);
+				break;
+			case "MMM-CasperValidator-OUR_NODE_STATUS":
+				this.sendNodeStatus(notification, payload);
+				break;
+			case "MMM-CasperValidator-REWARDS":
+				this.sendRewards(notification, payload.validatorAddress);
+				break;
+			case "MMM-CasperValidator-GET_AUCTION_INFO":
+				this.sendStakedInfo(payload);
+				break;
 		}
 	},
 
-	compareBothNodes: async function(payload) {
-		let theirNode = await this.getCasperRPC(payload.theirNode);
-		let ourNode = await this.getCasperRPC(payload.ourNode);
+	sendStakedInfo: async function(payload) {
+		let auctionInfo = await this.requestAuctionInfo(payload);
+		let bidData = auctionInfo.auction_state.bids.filter((obj) => obj.public_key == payload.validatorAddress)[0];
+		let selfStakedAmount = this.convertToCSPR(bidData.bid.staked_amount);
+		let delegatorStakedAmount = bidData.bid.delegators.map(a => this.convertToCSPR(a.staked_amount)).reduce((a, b) => a + b, 0);
 
-		this.sendSocketNotification("MMM-CasperValidator-COMPARE_NODES", {
-			theirNode: theirNode,
-			ourNode: ourNode
+		this.sendSocketNotification("MMM-CasperValidator-STAKED_INFO", {
+			selfStaked: selfStakedAmount,
+			totalStaked: selfStakedAmount + delegatorStakedAmount,
+			active: !bidData.bid.inactive
 		});
 	},
+
+	convertToCSPR: function(motes) {
+		return parseInt(motes) / 1e9;
+	},
+
+	requestAuctionInfo: function(payload) {
+		return axios.post(payload.nodeUrl, {
+			id: "0",
+			jsonrpc: "2.0",
+			method: "state_get_auction_info"
+		})
+		.then(res => res.data.result)
+		.catch(error => console.log(error));
+	},
+
+	sendNodeStatus: async function(notification, payload) {
+		let nodeStatus = await this.getCasperRPC(payload.nodeUrl);
+		this.sendSocketNotification(notification, nodeStatus);
+	},
+
+	sendRewards: function(notification, validatorAddress) {
+		var self = this;
+
+		axios.get(`https://event-store-api-clarity-mainnet.make.services/validators/${validatorAddress}/rewards?with_amounts_in_currency_id=1&page=1&limit=4&order_direction=DESC`)
+		.then(res => self.sendSocketNotification(notification, res.data) )
+		.catch(error => console.log(error));
+	},
+
 	getCasperRPC: function(url) {
 		var self = this;
 
@@ -52,5 +93,5 @@ module.exports = NodeHelper.create({
 			height: data.result.last_added_block_info.height,
 			next_upgrade: data.result.next_upgrade
 		}
-	}
+	},
 });
